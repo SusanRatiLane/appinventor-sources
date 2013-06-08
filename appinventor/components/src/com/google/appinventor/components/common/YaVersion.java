58208bc7d0fff0bfdda342c2bea6948e4be4845b
Jeffrey I. Schiller <jis@mit.edu>
2013-06-08 16:24:41 -0400

    MIT BRANDING COMMIT (ai2-test)
    MIT BRANDING COMMIT: This commit sets up the MIT App Inventor 2 Alpha
    Release (formerly "newblocks") branding.
    
    Change-Id: I9867dc59b6d87402f64df3e2ac02e783366a986a

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -1348,6 +1348,6 @@ public class YaVersion {
   public static final String [] ACCEPTABLE_COMPANIONS = { "2.57", "2.57u" };
 
   // Splash Screen Values
-  public static final int SPLASH_SURVEY = 1;
+  public static final int SPLASH_SURVEY = 0;
 
 }
