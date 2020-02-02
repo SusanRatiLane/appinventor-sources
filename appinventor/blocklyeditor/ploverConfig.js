SusanRatiLane/appinventor-translations

MIT BRANDING COMMIT (mit-appinventor-newblocks)
MIT BRANDING COMMIT: This commit sets up the MIT App Inventor 2 Alpha
Release (formerly "newblocks") branding. This commit is *ONLY* for
http://ai2.appinventor.mit.edu/ (app id mit-appinventor-newblocks)

Change-Id: I9867dc59b6d87402f64df3e2ac02e783366a986a

appinventor/blocklyeditor/ploverConfig.js
@@ -131,8 +131,8 @@
   // will be included, so when test-raw.html loads each input in RAW mode,
   // it is important that the proper charset be used.
   "output-charset": "UTF-8",
-  "mode": "RAW",
-//  "mode": "SIMPLE_OPTIMIZATIONS",
+//  "mode": "RAW",
+  "mode": "SIMPLE_OPTIMIZATIONS",
 //  "mode" : "WHITESPACE_ONLY",
   "closure-library" : "../lib/closure-library/closure/goog",
   "output-file": "../build/blocklyeditor/blockly-all.js"

SusanRatiLane/appinventor-sources

appinventor/blocklyeditor/ploverConfig.js
@@ -188,8 +188,8 @@
   // will be included, so when test-raw.html loads each input in RAW mode,
   // it is important that the proper charset be used.
   "output-charset": "UTF-8",
-  "mode": "RAW",
-//  "mode": "SIMPLE",
+//  "mode": "RAW",
+  "mode": "SIMPLE",
 //  "mode" : "WHITESPACE_ONLY",
   "experimental-compiler-options": {
     "languageIn": "ECMASCRIPT5"